fb33fd718cce007bd37177517b0aa8e37d686dff
balabhaskararo <balabhaskararoguntupalli@gmail.com>
2019-05-15 16:14:02 +0530

    sonarinstall script changedd

--- Sonar-install.sh
+++ Sonar-install.sh
@@ -2,8 +2,8 @@
 # resouce document https://directdevops.blog/2019/01/05/sonarqube/
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install openjdk-8jdk -y
-sudo sh -c 'echo deb [trusted=yes] http://downloads.sourceforge.net/project/sonar-pkg/deb binary/ > /etc/apt/sources.list'
+sudo apt-get install openjdk-8-jdk -y
+sudo sh -c 'echo deb [trusted=yes] http://downloads.sourceforge.net/project/sonar-pkg/deb binary/ > /etc/apt/sources.list.d/sonar.list'
 sudo apt-get update
 sudo apt-get install sonar=6.7.4 -y
 sudo systemctl enable sonar.service
